4c6762b9a8d10270cf617333c9912797c774b278
DragonCloudBurst <katieaspivey@gmail.com>
2025-11-28 18:26:54 -0500

    begin setup of main menu, add .gitignore

--- TwoDoTEST/Program.cs
+++ TwoDoTEST/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Spectre.Console;
 
-Console.WriteLine("Hello, World!");
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+	AnsiConsole.MarkupLine("[fuchsia]╭[/]               [fuchsia] ₊˚ ✧ ━━━━⊱⋆⊰━━━━ ✧ ₊˚[/]                [fuchsia]╮[/]");
+	AnsiConsole.MarkupLine("[fuchsia]│[/]                [aqua]⚞ Welcome to TwoDo! ⚟[/]                [fuchsia]│[/]");
+	AnsiConsole.MarkupLine("[fuchsia]│[/]          [aqua]What would you like TwoDo today?[/]           [fuchsia]│[/]");
+	AnsiConsole.MarkupLine("[fuchsia]│[/]                [fuchsia] ────── ⋆⋅☆⋅⋆ ──────[/]                 [fuchsia]│[/]");
+	AnsiConsole.MarkupLine("[fuchsia]│[/]   [grey89]☼ To see tasks, type 'task'[/]                       [fuchsia]│[/]");
+	AnsiConsole.MarkupLine("[fuchsia]│[/]   [grey89]☼ To create a new task, type 'new'[/]                [fuchsia]│[/]");
+    }
+
+}
+

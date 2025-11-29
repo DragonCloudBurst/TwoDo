903337c3df0aea62e7621a217be25bc95a8d86cc
DragonCloudBurst <katieaspivey@gmail.com>
2025-11-28 19:42:40 -0500

    fix Task.cs so the spaces will actually be added in

--- TwoDoTEST/Task.cs
+++ TwoDoTEST/Task.cs
@@ -19,6 +19,7 @@ public class Task
     {
 	int strLen = this.taskText.Length;
 	int spacesNeeded = maxWidth - strLen;
+	double spacesNeededHalf = spacesNeeded / 2;
 	int spacesNeededLeftSide = 0;
 	int spacesNeededRightSide = 0;
 
@@ -26,17 +27,20 @@ public class Task
 	if ((spacesNeeded / 2) % 2 != 0)
 	{
 	    // if the number needed is odd or not an integer
-            spacesNeededLeftSide = Math.Floor(spacesNeeded / 2);
-	    spacesNeededRightSide = (spacesNeeded / 2) + 1;
+            spacesNeededLeftSide = (int)(Math.Floor(spacesNeededHalf));
+	    spacesNeededRightSide = (int)(spacesNeededHalf + 1);
 	}
 	else
 	{
 	    // if the number needed is an even integer
-	    spacesNeededLeftSide = spacesNeeded / 2;
-	    spacesNeededRightSide = spacesNeeded / 2;
+	    spacesNeededLeftSide = (int)(spacesNeededHalf);
+	    spacesNeededRightSide = (int)(spacesNeededHalf);
 	}
 
-	return $"[fuchsia]│[/][grey89]☼ {" " * spacesNeededLeftSide}{this.taskText}[/]{" " * spacesNeededRightSide}[fuchsia]│[/]");
+	string leftSpaces = new string(' ', spacesNeededLeftSide);
+	string rightSpaces = new string(' ', spacesNeededRightSide);
+
+	return $"[fuchsia]│[/][grey89]☼ {leftSpaces}{this.taskText}[/]{rightSpaces}[fuchsia]│[/]";
     }
 
 }
